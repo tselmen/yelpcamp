ef2ed172b2c17ed3229b5be3b81c035035ee5681
Tselmen Tugsbayar <tugsbayartselmen@gmail.com>
2021-01-06 21:35:46 +0800

    Added basic campground model and connected to MongoDB

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express = require('express');
-const path = require('path')
+const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+    console.log("Database connected")
+});
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home')
-})
+});
+
+app.get('/makecamp', async (req, res) => {
+    const camp = new Campground({
+        title: "First camp",
+        description: "Test campground"
+    });
+    await camp.save();
+    res.send(camp);
+});
 
 app.listen(3000, () => {
     console.log("Server started on port 3000")
-})
+});
