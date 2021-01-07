0a640566039a74312ec6781f81c15b201dadc75f
Tselmen Tugsbayar <tugsbayartselmen@gmail.com>
2021-01-07 16:34:58 +0800

    Added basic styling with bootstrap

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -19,6 +20,7 @@ db.once('open', () => {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', ejsMate);
 
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random].city}, ${cities[random].state}`
+            location: `${cities[random].city}, ${cities[random].state}`,
+            image: 'https://images.unsplash.com/photo-1484190812281-2387e3ae33a5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080',
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, quaerat ex a debitis tempora amet exercitationem dolor obcaecati sunt nam provident totam delectus, similique magnam! Eveniet sit tempore distinctio tempora?',
+            price: price
         });
         await camp.save();
     }
